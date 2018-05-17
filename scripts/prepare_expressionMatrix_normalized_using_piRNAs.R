77a847b57a5e7b9a2dd311516eeebde480cc9c2b
jingkui <jingkui.wang@imp.ac.at>
2018-05-17 09:14:00 +0200

    first version of scripts/prepare_expressionMatrix_normalized_using_piRNAs.R

---
+++ scripts/prepare_expressionMatrix_normalized_using_piRNAs.R
@@ -0,0 +1,155 @@
+##########################################################################
+##########################################################################
+## Project: Chiara's neuron class-specific miRNA expression  
+## Script purpose: to test piRNA normalization for the expression matrix in the decomvolution analysis
+## Usage example: 
+## Author: Jingkui Wang (jingkui.wang@imp.ac.at)
+## Date of creation: Wed May 16 13:58:52 2018
+##########################################################################
+##########################################################################
+RdataDir = paste0("../results/miRNAs_neurons_v1_2018_03_07/Rdata/")
+statDir = "../data/normalized_piRNAs"
+version.table = "miRNAs_neurons_v1_2018_03_07"
+resDir = "../results/tables_for_decomvolution"
+if(!dir.exists(resDir)) dir.create(resDir)
+
+######################################
+######################################
+## Section: load tables and mapping statistics for piRNA and siRNAs
+######################################
+######################################
+load(file = paste0(RdataDir, 'Design_Raw_readCounts_', version.table, '.Rdata'))
+source("miRNAseq_functions.R")
+
+Filter.lowly.expressed.using.predefined.miRNA.list = TRUE;
+Merge.techinical.replicates.N2 = TRUE
+
+tcs = unique(design$tissue.cell)
+tcs = setdiff(tcs, c("whole.body"))
+length(tcs)
+
+if(Merge.techinical.replicates.N2){
+  rep.technical = list(c("57751", "57753"), c("57752", "57754"))
+  for(n in 1:length(rep.technical))
+  {
+    index = c()
+    for(id in rep.technical[[n]])
+    {
+      #print(id)
+      index = c(index, which(design$SampleID==id))
+    }
+    
+    design$SampleID[index[1]] = paste0(design$SampleID[index], collapse = ".")
+    ss = apply(all[, (index+1)], 1, function(x) sum(x, na.rm = TRUE))
+    all[, (index[1]+1)] = ss;
+    colnames(all)[(index[1]+1)] = paste0(design$genotype[index[1]], "_", design$tissue.cell[index[1]], "_", design$treatment[index[1]], "_",  design$SampleID[index[1]])
+    design = design[-index[-1], ]
+    all = all[, -(index[-1]+1)]
+  }
+} 
+
+# filter lowly expressed miRNA with list of predefined miRNAs that were identified using all untreated samples 
+if(Filter.lowly.expressed.using.predefined.miRNA.list){
+  list.expressed = read.csv(paste0("../data/list_expressed_miRNAs_using_Untreated_samples_Henn1_mutant_WT_all_cpm_10.csv"), header = TRUE, as.is = c(1, 2))
+  # prepare old llist
+  list.expressed = find.mature.ones.for.expressed.miRNAs(list.expressed)
+  expressed.miRNAs = data.frame(list.expressed[, c(1:3)], stringsAsFactors = FALSE)
+}
+
+####################
+## calculate cpm and select only expressed miRNAs and start to use the gene names instead of arms 
+####################
+raw = as.matrix(all[, -1])
+raw[which(is.na(raw))] = 0
+raw = floor(raw)
+rownames(raw) = all$gene
+cpm = my.cpm.normalization(raw)
+
+expressed.miRNAs = expressed.miRNAs[expressed.miRNAs$mature,]
+mm = match(expressed.miRNAs$miRNA, all$gene)
+cpm = cpm[mm, ]
+rownames(cpm) = expressed.miRNAs$gene
+
+## filter the samples unrelevant (non-neuron samples, henn1-mutant background)
+kk = which(design$tissue.cell=="Glial.cells" | design$tissue.cell == "CEPsh" | design$genotype == "henn1.mutant")
+design.matrix = design[-kk, ]
+cpm = cpm[, -kk]
+
+######################################
+######################################
+## Section: calculate scaling factors using piRNAs or siRNAs and scale cpm using them and test if it works
+######################################
+######################################
+stat.list = list.files(path = statDir, pattern = "*_cnt.typeHierarchy.txt", full.names = TRUE)
+stats = NULL;
+for(n in 1:length(stat.list))
+{
+  if(n ==1){
+    stats = read.table(stat.list[n], sep = "\t", header=TRUE)
+  }else{
+    test = read.table(stat.list[n], sep = "\t", header=TRUE)
+    stats = data.frame(stats, test[, -1])
+  }
+}
+stats = stats[which(!is.na(stats$type)==TRUE), ]
+rownames(stats) = stats[, 1]
+stats = stats[, -1]
+
+colnames(stats) = sapply(colnames(stats), function(x) gsub('count.', '', x), USE.NAMES = FALSE)
+
+## need to merge again the techinical replicates for N2
+if(Merge.techinical.replicates.N2){
+  rep.technical = list(c("57751", "57753"), c("57752", "57754"))
+  for(n in 1:length(rep.technical))
+  {
+    index = c()
+    for(id in rep.technical[[n]])
+    {
+      #print(id)
+      index = c(index, which(colnames(stats)==id))
+    }
+    
+    #design$SampleID[index[1]] = paste0(design$SampleID[index], collapse = ".")
+    ss = apply(stats[, index], 1, function(x) sum(x, na.rm = TRUE))
+    stats[, (index[1])] = ss;
+    colnames(stats)[index[1]] = paste0(colnames(stats)[index], collapse = ".")
+      #paste0(design$genotype[index[1]], "_", design$tissue.cell[index[1]], "_", design$treatment[index[1]], "_",  design$SampleID[index[1]])
+    #design = design[-index[-1], ]
+    stats = stats[, -index[-1]]
+  }
+} 
+
+mm = match(design.matrix$SampleID, colnames(stats))
+
+stats = stats[, mm]
+colnames(stats) = paste0(design.matrix$genotype, "_", design.matrix$tissue.cell, "_", design.matrix$treatment, "_", design.matrix$SampleID)
+stats = data.frame(t(stats))
+
+plot(stats$piRNA, stats$ncRNA, log='xy')
+abline(0, 1, lwd=2.0, col='red')
+
+sizefactors.piRNA = stats$piRNA/median(stats$piRNA) 
+#sizefactors.siRNA = stats$siRNA/median(stats$siRNA)
+#sizefactors = (sizefactors.piRNA + sizefactors.siRNA) /2
+sizefactors = sizefactors.piRNA
+cpm.piRNA = cpm
+for(n in 1:ncol(cpm.piRNA))
+{
+  cpm.piRNA[,n] = cpm[,n]/sizefactors[n]
+}
+
+cpm.piRNA[which(rownames(cpm.piRNA)=='lsy-6'), grep('treated', colnames(cpm.piRNA))]
+par(mfrow=c(1,3))
+plot(cpm.piRNA[, c(2,1)], log='xy'); 
+points(c(10^-5, 10^8), c(10^-5, 10^8), lwd=2.0, col='red', type = "l")
+
+plot(cpm.piRNA[, c(24,16)], log='xy'); 
+points(c(10^-5, 10^8), c(10^-5, 10^8), lwd=2.0, col='red', type = "l")
+
+plot(cpm.piRNA[, c(32,36)], log='xy'); 
+points(c(10^-5, 10^8), c(10^-5, 10^8), lwd=2.0, col='red', type = "l")
+
+
+
+
+
