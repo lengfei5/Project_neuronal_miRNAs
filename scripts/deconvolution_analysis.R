fc7a81c3619bef023ed9ee79927520826438db25
jingkui <jingkui.wang@imp.ac.at>
2018-08-02 14:23:20 +0200

    first time finish the glmnet fitting code for gene examples

--- scripts/deconvolution_analysis.R
+++ scripts/deconvolution_analysis.R
@@ -14,12 +14,15 @@ version.ExprsMatrix = "miRNAs_neurons_v1_2018_03_07"
 version.Fraction.Matrix = "_miRNAs_neurons_20180525"
 version.EnrichscoreMatrix = "20180506"
 
+version.analysis = "_20180802"
+
 resDir = "../results/decomvolution_results"
 if(!dir.exists(resDir)) dir.create(resDir)
 
 fitting.space = "linear" ## linear or log2 transformed for expression matrix
-Use.mergedFractionMatrix = TRUE
-Use.mergedExpressionMatrix = FALSE
+Use.mergedFractionMatrix = TRUE 
+Use.mergedExpressionMatrix = FALSE # group the genes if they show similar gene expression pattern
+Manually.unifiy.sample.names.forMatrix = TRUE
 Check.ProprotionMatrix.ExpressionMatrix = FALSE
 
 ######################################
@@ -57,10 +60,6 @@ if(Use.mergedFractionMatrix){
 ######################################
 ######################################
 if(!Use.mergedExpressionMatrix){
-  load(file = paste0(RdataDir, "Enrichscores_Matrix_13samples_selected_and_all_genes_", version.EnrichscoreMatrix, ".Rdata"))
-  enriched.list = colnames(enrich.matrix.sel)
-  #enriched.list = sapply(enriched.list, function(x) gsub("[.]", "-", x), USE.NAMES = FALSE)
-  
   load(file = paste0(RdataDir, 'piRANormalized_cpm.piRNA_batchCorrectedCombat_reAveraged_', version.ExprsMatrix, '.Rdata'))
   jj = grep('_untreated', colnames(cpm.piRNA.bc.meanrep))
   total = apply(cpm.piRNA.bc.meanrep[, jj], 1, median)
@@ -75,7 +74,6 @@ if(!Use.mergedExpressionMatrix){
   # this transformation is to scale the range for each gene so that all data fall into the same range and e' should still follow the positive constrain 
   # meanwhile using ratio between expression and background to filter non-expressed ones  
   ####################
-  
   if(fitting.space == "linear")
   {
     expression = xx[, -c(1)] # ignore the gene expression in the whole body
@@ -85,7 +83,6 @@ if(!Use.mergedExpressionMatrix){
   }
     
 }else{
-  
   source('miRNAseq_functions.R')
   newExprM = expressionMatrix.grouping(xx) 
 }
@@ -97,12 +94,50 @@ if(!Use.mergedExpressionMatrix){
 # especailly for the fitting in the linear scale
 ########################################################
 ########################################################
-mm = match((enriched.list), rownames(expression))
-expression.sel = t(expression[mm, ])
+###############################
+# select samples to use and also match sample orders in matrix A and Y 
+# mannually unify the names of each sample A and Y 
+###############################
+if(Manually.unifiy.sample.names.forMatrix){
+  rownames(proportions) = c("Dopaminergic","Serotonergic","Glutamatergic", "Cholinergic", "GABAergic",  "Ciliatedsensory",
+                            "Mechanosensory", "unc.86", "Pharyngeal", "ceh.14", "unc.42", "unc.3","ASE", "Pan.neurons")
+  if(fitting.space == "linear"){
+    expression = t(expression)
+    rownames(expression) = c( "background", "ASE", "Serotonergic", "Dopaminergic", "Glutamatergic", "Ciliatedsensory",
+                                  "GABAergic", "Mechanosensory", "unc.86", "Pharyngeal", "Cholinergic", "ceh.14", "unc.42")
+    yy = proportions;
+    yy = rbind(rep(0, ncol(proportions)), proportions)
+    yy = cbind(rep(1, nrow(yy)), yy)
+    colnames(yy) = c("C0", colnames(proportions))
+    rownames(yy) = c("background", rownames(proportions))
+    proportions = yy
+    
+    save(expression, proportions, file = paste0(RdataDir, "Expression_Fraction_Matrix_withBackground_cleaed", 
+                                                version.Fraction.Matrix,"_", version.ExprsMatrix, ".Rdata"))
+    
+  }else{
+    rownames(expression.sel) = c("ASE", "Serotonergic", "Dopaminergic", "Glutamatergic", "Ciliatedsensory",
+                                  "GABAergic", "Mechanosensory", "unc.86", "Pharyngeal", "Cholinergic", "ceh.14", "unc.42")
+    
+  }
+  #index.sel = c(13, 2, 1, 3, 6, 5, 7, 8, 9, 4, 10, 11)
+  #proportions.sel = proportions[index.sel, ]
+}
+
+###############################
+# select genes of intereset and match sample orders
+###############################
+load(file = paste0(RdataDir, "Expression_Fraction_Matrix_withBackground_cleaed.Rdata"))
+load(file = paste0(RdataDir, "Enrichscores_Matrix_13samples_selected_and_all_genes_", version.EnrichscoreMatrix, ".Rdata"))
+enriched.list = colnames(enrich.matrix.sel)
+#enriched.list = sapply(enriched.list, function(x) gsub("[.]", "-", x), USE.NAMES = FALSE)
+
+mm = match((enriched.list), colnames(expression))
+expression.sel = expression[, mm]
 #expression.sel = log2(expression.sel)
 
-index.sel = c(13, 2, 1, 3, 6, 5, 7, 8, 9, 4, 10, 11)
-proportions.sel = proportions[index.sel, ]
+sels = match(rownames(expression.sel), rownames(proportions))
+proportions.sel = proportions[sels, ]
 
 ####################
 ## double check the proprotion matrix and expression matrix
@@ -110,19 +145,24 @@ proportions.sel = proportions[index.sel, ]
 ## now manually (to change)
 ####################
 if(Check.ProprotionMatrix.ExpressionMatrix){
-  xx = proportions;
+  xx = proportions.sel;
   xx[which(xx>0)] = 1
-  yy = expression.sel[c(3, 2, 4, 10, 6, 5, 7, 8, 9, 11, 12, 1), ];
+  yy = expression.sel;
+  #yy = expression.sel[c(3, 2, 4, 10, 6, 5, 7, 8, 9, 11, 12, 1), ];
   #yy = proportions[c(index.sel, 12, 14), ]
   
   library("pheatmap")
   library("RColorBrewer")
   
-  pdfname = paste0(resDir, "/heatmap_for_ProportionaMatrix_14_samples_96_neuronClasses_ExpressionMatrix", ".pdf")
+  pdfname = paste0(resDir, "/Heatmap_Proportiona_Expression_Matrix", version.analysis, ".pdf")
   pdf(pdfname, width=15, height = 6)
   par(cex =0.7, mar = c(3,3,2,0.8)+0.1, mgp = c(1.6,0.5,0),las = 0, tcl = -0.3)
   par(mfrow=c(1, 1))
   # par(mfcol=c(1, 1))
+  
+  if(fitting.space == 'linear') {logaxis = 'xy'; yy = t(log2(t(yy)/yy[which(rownames(yy)=='background'), ]));
+  }else{logaxis = ''}
+  
   
   pheatmap(xx, cluster_rows=FALSE, show_rownames=TRUE, show_colnames = TRUE,
            cluster_cols=TRUE, 
@@ -133,13 +173,11 @@ if(Check.ProprotionMatrix.ExpressionMatrix){
            color = colorRampPalette(rev(brewer.pal(n = 7, name="RdYlBu")))(100))
   
   ## double check the expression matrix
-  if(fitting.space == 'linear') {logaxis = 'xy';
-  }else{logaxis = ''}
-  
+
   par(mfrow= c(1:2))
-  plot(t(expression.sel[match(c("Dopaminergic", "Ciliated.sensory"), rownames(expression.sel)), ]), log=logaxis)
+  plot(t(expression.sel[match(c("Dopaminergic", "Ciliatedsensory"), rownames(expression.sel)), ]), log=logaxis)
   abline(0, 1, lwd=2.0, col='red')
-  plot(t(expression.sel[match(c("mechanosensory",  "unc.86.expressing"), rownames(expression.sel)), ]), log=logaxis)
+  plot(t(expression.sel[match(c("Mechanosensory",  "unc.86"), rownames(expression.sel)), ]), log=logaxis)
   abline(0, 1, lwd=2.0, col='red')
   
   dev.off()
@@ -148,71 +186,89 @@ if(Check.ProprotionMatrix.ExpressionMatrix){
 
 ######################################
 ######################################
-## Section: process the table and run the glmnet
+# Section: process the table and run the glmnet
+# the glmnet will be run for each gene, because the group lasso is not desirable
 ######################################
 ######################################
 require(glmnet)
 x=as.matrix(proportions.sel)
 y = as.matrix(expression.sel)
 
-x.ms = apply(x, 2, sum)
-x = x[, which(x.ms>0)]
+Example2test = c("lsy-6", "mir-791", "mir-793", "mir-790", "mir-1821", "mir-83", "mir-124")
+jj2test = match(Example2test, colnames(y))
+y = y[, jj2test[which(!is.na(jj2test)==TRUE)]]
 
+res = matrix(NA, nrow = ncol(x), ncol = ncol(y)) 
+colnames(res) = colnames(y)
+rownames(res) = colnames(x)
+#x.ms = apply(x, 2, sum)
+#x = x[, which(x.ms>0)]
 #x = x>0
+
 #y=cbind(y1,y2)
 #rownames(y) = res.sel$gene
 
-intercept=0
+intercept=FALSE
 standardize=FALSE ### standardize matrix of motif occurrence makes more sense because the absolute number of motif occurrence is not precise.
 standardize.response=FALSE
-alpha = 0.5
+alpha = 0.005 # 0.001 is the default value
 grouped = FALSE
 
-cv.fit=cv.glmnet(x, y, family='mgaussian', grouped=grouped, alpha=alpha, nlambda=500, standardize=standardize, 
-                 standardize.response=standardize.response, intercept=intercept)
-par(mfrow= c(1,1))
-plot(cv.fit)
-
-#optimal = which(cv.fit$lambda==cv.fit$lambda.min)
-#optimal = which(cv.fit$lambda==cv.fit$lambda.1se)
-fit=glmnet(x,y, alpha=alpha, lambda=cv.fit$lambda,family='mgaussian', 
-            standardize=standardize, standardize.response=standardize.response, intercept=intercept)
-
-#optimal = which(fit$df<=70)
-#optimal = max(optimal)
-optimal = which(cv.fit$lambda==cv.fit$lambda.min)
-
-## Adapted from @Mehrad Mahmoudian:
-myCoefs <- coef(fit, s=cv.fit$lambda.min);
-#myCoefs[which(myCoefs != 0 ) ]               #coefficients: intercept included
-## [1]  1.4945869 -0.6907010 -0.7578129 -1.1451275 -0.7494350 -0.3418030 -0.8012926 -0.6597648 -0.5555719
-## [10] -1.1269725 -0.4375461
-#myCoefs@Dimnames[[1]][which(myCoefs != 0 ) ] #feature names: intercept included
-## [1] "(Intercept)" "feature1"    "feature2"    "feature3"    "feature4"    "feature5"    "feature6"   
-## [8] "feature7"    "feature8"    "feature9"    "feature10"  
-
-## Asseble into a data.frame
-#myResults <- data.frame(
-#  features = myCoefs@Dimnames[[1]], #intercept included
-#  coefs    = myCoefs              #intercept included
-#)
-
-#myResults
-##       features      coefs
-
-## extract the fitting result
-#colnames(x)[which(fit$beta[[1]][,optimal]!=0)]
-res = matrix(NA, nrow = ncol(x), ncol = ncol(y))
-colnames(res) = colnames(y)
-rownames(res) = colnames(x)
-for(n in 1:ncol(res))
+for(n in 1:nrow(y))
 {
-  res[,n] = fit$beta[[n]][,optimal]
+  # n = 2
+  cv.fit=cv.glmnet(x, y[,n], family='gaussian', alpha=alpha, nlambda=200, standardize=standardize, lower.limits = 0,
+                   standardize.response=standardize.response, intercept=intercept, grouped = FALSE)
+  
+  fit=glmnet(x,y[,n], alpha=alpha, lambda=cv.fit$lambda,family='gaussian', lower.limits = 0,
+             standardize=standardize, standardize.response=standardize.response, intercept=intercept)
+  
+  #optimal = which(fit$df<=70)
+  #optimal = max(optimal)
+  #optimal = which(cv.fit$lambda==cv.fit$lambda.min)
+  ## Adapted from @Mehrad Mahmoudian:
+  myCoefs <- coef(fit, s=cv.fit$lambda.min);
+  par(mfrow= c(1,1))
+  #plot(cv.fit)
+  plot(fit, label = TRUE)
+  plot(fit, xvar = "lambda", label = TRUE); abline(v=log(cv.fit$lambda.min))
+  res[,n] = as.numeric(myCoefs)[-1]
+  
+  cat("---------------\n", colnames(res)[n], ":\n", 
+      paste0(rownames(res)[which(res[,n]>0)], collapse = "\n"), "\n",
+      "---------------\n")
+    
+  #coef(cv.fit, s = "lambda.min")
+  #myCoefs[which(myCoefs != 0 ) ]               #coefficients: intercept included
+  ## [1]  1.4945869 -0.6907010 -0.7578129 -1.1451275 -0.7494350 -0.3418030 -0.8012926 -0.6597648 -0.5555719
+  ## [10] -1.1269725 -0.4375461
+  #myCoefs@Dimnames[[1]][which(myCoefs != 0 ) ] #feature names: intercept included
+  ## [1] "(Intercept)" "feature1"    "feature2"    "feature3"    "feature4"    "feature5"    "feature6"   
+  ## [8] "feature7"    "feature8"    "feature9"    "feature10"  
+  
+  ## Asseble into a data.frame
+  #myResults <- data.frame(
+  #  features = myCoefs@Dimnames[[1]], #intercept included
+  #  coefs    = myCoefs              #intercept included
+  #)
+  
+  #myResults
+  ##       features      coefs
+  
+  ## extract the fitting result
+  #colnames(x)[which(fit$beta[[1]][,optimal]!=0)]
+  #res = matrix(NA, nrow = ncol(x), ncol = ncol(y))
+  #colnames(res) = colnames(y)
+  #rownames(res) = colnames(x)
+  #for(n in 1:ncol(res))
+  #{
+  #  res[,n] = fit$beta[[n]][,optimal]
+  #}
+  #res = data.frame(res)
+  #cbind(rownames(res)[order(-res$lsy.6)],  res$lsy.6[order(-res$lsy.6)])
 }
 
-res = data.frame(res)
 
-cbind(rownames(res)[order(-res$lsy.6)],  res$lsy.6[order(-res$lsy.6)])
 
 
 
