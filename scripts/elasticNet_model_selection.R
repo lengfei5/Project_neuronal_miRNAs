711433d504c90a6bece3d9dc3051f24a560d0768
jingkui <jingkui.wang@imp.ac.at>
2018-09-19 16:29:07 +0200

    update EBIC and HBIC in the model selection methods

--- scripts/elasticNet_model_selection.R
+++ scripts/elasticNet_model_selection.R
@@ -1,37 +1,61 @@
+##########################################################################
+##########################################################################
+# Project:
+# Script purpose: to select the optimal model using BIC, AIC, AICc and HQC for elastic-net
+# Usage example: 
+# Author: Jingkui Wang (jingkui.wang@imp.ac.at)
+# Date of creation: Wed Sep 19 11:09:08 2018
+##########################################################################
+##########################################################################
 feature.selection.and.refitting.ols = function(fit, x, y)
 {
   # y = y[,n]
-  nb.data = length(y)
+  n.data = length(y)
   #n=length(y)
   list.lambda = fit$lambda;
-  bic=rep(NA, length(list.lambda));
-  aic=bic;
-  aicc = bic; 
-  hqc=bic;
+  crits = c("mse", "BIC", "AIC", "AICc","EBIC", "HBIC", "HQC", "HBIC2", "HBIC3")
+  beta = matrix(0, nrow = length(list.lambda), ncol = (ncol(x)+length(crits)))
+  #rownames(beta) = paste0(list.lambda);
+  colnames(beta) = c(colnames(x), crits)
+  
   for(n in 1:length(list.lambda))
   {
-    # n = 20;
+    # n = 40;
     #df = fit$df[n]
     cfs = as.numeric(coef(fit, s=list.lambda[n]))
     cfs = cfs[-1] # move the intercept
     feature.sels = which(cfs>0)
     names(feature.sels) = colnames(x)[feature.sels]
+    
     if(length(feature.sels)==0) {
-      mse = sum(y-mean(y)^2);
+      mse = sum((y-mean(y))^2);
     }else{
       x.sel = x[, feature.sels]
-      ff = lm(y ~ x.sel)
-      mse = sum(ff$residuals^2)
+      
+      if(length(feature.sels)>=2){
+        x.sel.y = data.frame(y, x.sel)
+        model =  eval(parse(text = paste0("y ~ ", paste0(colnames(x.sel), collapse = " + "))))
+        ff = lm(model, data = x.sel.y)
+      }else{
+        ff = lm(y ~ x.sel)
+      }
+      mse = sum(ff$residuals^2)/n.data
+      beta[n, feature.sels] = as.numeric(coefficients(ff))[-1]
     }
     
     nvar = fit$df[n] + 1
-    bic = nb.data*log(mse)+nvar*log(nb.data)
-    aic = nb.data*log(mse)+2*nvar
-    aicc = aic+(2*nvar*(nvar+1))/(nb.data-nvar-1)
-    hqc = nb.data*log(mse)+2*nvar*log(log(nb.data))
+    p_full = ncol(x)
+    beta[n, (ncol(x)+1)] = mse
+    beta[n, (ncol(x)+2)] = n.data*log(mse) + nvar*log(n.data) # bic
+    beta[n, (ncol(x)+3)] = n.data*log(mse) + 2*nvar # aic
+    beta[n, (ncol(x)+4)] = n.data*log(mse) + 2*nvar + (2*nvar*(nvar+1))/(n.data-nvar-1) # aicc
+    beta[n, (ncol(x)+5)] = n.data*log(mse) + log(nvar)*p_full# ebic
+    beta[n, (ncol(x)+6)] = n.data*log(mse) + 2*log(nvar)*p_full# hbic
+    beta[n, (ncol(x)+7)] = n.data*log(mse) + 2*nvar*log(log(n.data))
     
   }
   
+  beta = data.frame(beta)
   #plot(fit, label = TRUE)
   #plot(fit, xvar = "lambda", label = TRUE); abline(v=log(cv.fit$lambda.min))
   
@@ -41,23 +65,7 @@ feature.selection.and.refitting.ols = function(fit, x, y)
   #rr.coefs = coefs/max(coefs)
   #coefs[which(rr.coefs<0.05)] = 0
   
-  #model = glmnet(x = x, y = y, ...)
-  #coef = coef(model)
-  #lambda = model$lambda
-  #df = model$df
-  
-  #yhat=cbind(1,x)%*%coef
-  #residuals = (y- yhat)
-  #mse = colMeans(residuals^2)
-  #sse = colSums(residuals^2)
-  
- 
-  
-  sst = (n-1)*var(y)
-  r2 = 1 - (sse/sst)
-  adjr2 = (1 - (1 - r2) * (n - 1)/(nrow(x) - nvar - 1))
-  
-  crit=switch(crit,bic=bic,aic=aic,aicc=aicc,hqc=hqc)
+  #crit=switch(crit,bic=bic,aic=aic,aicc=aicc,hqc=hqc)
   
   selected=best.model = which(crit == min(crit))
   
